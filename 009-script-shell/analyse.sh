bc5a45e5af6be337f2c51f7fec4b2fdb88b5d58f
moh4ndis <elanaamimohamed@gmail.com>
2025-05-14 02:23:34 +0100

    analyse

--- 009-script-shell/analyse.sh
+++ 009-script-shell/analyse.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# Affiche le nombre de paramètres
-echo "Bonjour, vous avez rentré $# paramètres."
+# Affiche le nombre de parametres
+echo "Bonjour, vous avez rentre $# parametres."
 
 # Affiche le nom du script
 echo "Le nom du script est $0"
 
-# Affiche le 3ème paramètre (s'il existe)
+# Affiche le 3eme parametre (s'il existe)
 if [ $# -ge 3 ]; then
-    echo "Le 3ème paramètre est $3"
+    echo "Le 3eme parametre est $3"
 else
-    echo "Le 3ème paramètre est inexistant"
+    echo "Le 3eme parametre est inexistant"
 fi
 
-# Affiche la liste des paramètres
-echo "Voici la liste des paramètres : $@"
+# Affiche la liste des parametres
+echo "Voici la liste des parametres : $@"
