d04941c4f7388e939ecdd2471ae018814f7c9249
moh4ndis <elanaamimohamed@gmail.com>
2025-05-14 02:24:38 +0100

    backup

--- 009-script-shell/backup.sh
+++ 009-script-shell/backup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Nom du répertoire de sauvegarde
+# Nom du repertoire de sauvegarde
 BACKUP_DIR="OLD"
 
 # Format de la date
 DATE=$(date +%F)  # Format YYYY-MM-DD
 
-# Vérifier si le répertoire OLD existe, sinon le créer
+# Verifier si le repertoire OLD existe, sinon le creer
 if [ ! -d "$BACKUP_DIR" ]; then
     mkdir "$BACKUP_DIR"
     if [ $? -ne 0 ]; then
-        echo "Erreur : impossible de créer le répertoire $BACKUP_DIR"
+        echo "Erreur : impossible de creer le repertoire $BACKUP_DIR"
         exit 1
     fi
 fi
 
-# Parcourir les fichiers du répertoire courant (hors répertoires et OLD)
+# Parcourir les fichiers du repertoire courant (hors repertoires et OLD)
 for file in *; do
     if [ -f "$file" ] && [ "$file" != "$0" ]; then
         cp "$file" "$BACKUP_DIR/${file}#${DATE}"
     fi
 done
 
-echo "Sauvegarde terminée avec succès dans le répertoire '$BACKUP_DIR'."
+echo "Sauvegarde terminee avec succes dans le repertoire '$BACKUP_DIR'."
