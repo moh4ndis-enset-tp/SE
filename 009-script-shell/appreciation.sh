e35b7e17c3c76c7be1916900ca3420e20f289cc0
moh4ndis <elanaamimohamed@gmail.com>
2025-05-14 02:24:26 +0100

    appreciation

--- 009-script-shell/appreciation.sh
+++ 009-script-shell/appreciation.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Fonction pour afficher l'appréciation en fonction de la note
+# Fonction pour afficher l'appreciation en fonction de la note
 afficher_appreciation() {
     note=$1
     
-    # Vérifier si la note est un nombre
+    # Verifier si la note est un nombre
     if ! [[ $note =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
         echo "Erreur: Veuillez entrer un nombre valide."
         return
     fi
     
-    # Déterminer l'appréciation en fonction de la note
+    # Determiner l'appreciation en fonction de la note
     if (( $(echo "$note > 20 || $note < 0" | bc -l) )); then
         echo "Erreur: La note doit être comprise entre 0 et 20."
     elif (( $(echo "$note >= 16 && $note <= 20" | bc -l) )); then
-        echo "très bien"
+        echo "tres bien"
     elif (( $(echo "$note >= 14 && $note < 16" | bc -l) )); then
         echo "bien"
     elif (( $(echo "$note >= 12 && $note < 14" | bc -l) )); then
@@ -31,14 +31,14 @@ while true; do
     echo "Entrez une note (ou 'q' pour quitter) : "
     read saisie
     
-    # Vérifier si l'utilisateur souhaite quitter
+    # Verifier si l'utilisateur souhaite quitter
     if [ "$saisie" = "q" ] || [ "$saisie" = "Q" ]; then
         echo "Au revoir!"
         break
     fi
     
-    # Afficher l'appréciation pour la note saisie
+    # Afficher l'appreciation pour la note saisie
     appreciation=$(afficher_appreciation "$saisie")
-    echo "Appréciation: $appreciation"
+    echo "Appreciation: $appreciation"
     echo "-----------------------"
 done
